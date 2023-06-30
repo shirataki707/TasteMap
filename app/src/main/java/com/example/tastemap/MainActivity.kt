1ca61b8b84fadd4eb2103bf8c646f7a8a46fed4b
stakasaki <stakasaki707@gmail.com>
2023-07-01 06:15:38 +0900

    update splash animation

--- app/src/main/java/com/example/tastemap/MainActivity.kt
+++ app/src/main/java/com/example/tastemap/MainActivity.kt
@@ -5,12 +5,21 @@ import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.core.animation.doOnEnd
-import com.example.tastemap.ui.theme.TasteMapTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -18,39 +27,73 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TasteMapTheme {
-                TasteMapApp()
+
+        // [NOTE] Android11でデフォルトのTopBarを非表示にする，もっと良いやり方ありそう
+        theme.applyStyle(
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                R.style.Theme_TasteMap
+            } else {
+                com.airbnb.lottie.R.style.Theme_AppCompat_Light_NoActionBar
+            },
+            true
+        )
+
+        // Android12以上なら独自のSplash画面を表示
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashScreen = installSplashScreen()
+
+            splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+                val splashScreenView = splashScreenViewProvider.iconView
+
+                // アイコンを回転
+                val rotationAnimator = ObjectAnimator.ofFloat(splashScreenView, View.ROTATION, 0f, 360f).apply {
+                    duration = 800L
+                }
+
+                // スケール変更
+                val scaleAnimatorX= ObjectAnimator.ofFloat(splashScreenView, View.SCALE_X, 1f, 0f).apply {
+                    duration = 200L
+                }
+                val scaleAnimatorY = ObjectAnimator.ofFloat(splashScreenView, View.SCALE_Y, 1f, 0f).apply {
+                    duration = 200L
+                }
+
+                // フェードアウト
+                val fadeOutAnimator = ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f).apply {
+                    duration = 200L
+                    interpolator = AccelerateInterpolator()
+                    doOnEnd { splashScreenViewProvider.remove() }
+                }
+
+                // run the animations in sequence
+                AnimatorSet().apply {
+                    playSequentially(rotationAnimator, AnimatorSet().apply {
+                        playTogether(scaleAnimatorX, scaleAnimatorY, fadeOutAnimator)
+                    })
+                    start()
+                }
             }
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            // Android 12 (APIレベル 31) 以上の場合に実行するコード
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                val animatorSet = AnimatorSet()
-                animatorSet.play(
-                    ObjectAnimator.ofFloat(
-                        splashScreenView,
-                        View.SCALE_X,
-                        1f,
-                        20f,
-                    )
-                ).with(
-                    ObjectAnimator.ofFloat(
-                        splashScreenView,
-                        View.SCALE_Y,
-                        1f,
-                        20f,
-                    )
-                )
-                animatorSet.duration = 800L
-
-                animatorSet.doOnEnd { splashScreenView.remove() }
-
-                animatorSet.start()
-            }
-        } else {
-            // Android 11 (APIレベル 30) 以下の場合に実行するコード
+        setContent {
+            TasteMapApp()
         }
     }
 }
+
+@Composable
+fun SplashScreen(onAnimationFinished: () -> Unit) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.splash))
+    val progress by animateLottieCompositionAsState(composition)
+
+    LottieAnimation(
+        composition = composition,
+        progress = progress
+    )
+
+    LaunchedEffect(Unit) {
+        delay(5000L)  // Delay for 5 seconds
+        onAnimationFinished()
+    }
+}
+
