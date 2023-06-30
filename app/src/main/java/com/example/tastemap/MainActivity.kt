872e9c85af96f63c589cd700d5846600f8d92f2a
stakasaki <stakasaki707@gmail.com>
2023-07-01 07:39:31 +0900

    add permission request

--- app/src/main/java/com/example/tastemap/MainActivity.kt
+++ app/src/main/java/com/example/tastemap/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tastemap
 
+import android.Manifest
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Build
@@ -8,11 +9,14 @@ import android.view.View
 import android.view.animation.AccelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreenViewProvider
 import com.example.tastemap.ui.theme.TasteMapTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -20,6 +24,9 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 位置情報のパーミッション確認
+        confirmPermission()
 
         // [NOTE] Android11でデフォルトのTopBarを非表示にする，もっと良いやり方ありそう
         theme.applyStyle(
@@ -36,35 +43,7 @@ class MainActivity : ComponentActivity() {
             val splashScreen = installSplashScreen()
 
             splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
-                val splashScreenView = splashScreenViewProvider.iconView
-
-                // アイコンを回転
-                val rotationAnimator = ObjectAnimator.ofFloat(splashScreenView, View.ROTATION, 0f, 360f).apply {
-                    duration = 800L
-                }
-
-                // スケール変更
-                val scaleAnimatorX= ObjectAnimator.ofFloat(splashScreenView, View.SCALE_X, 1f, 0f).apply {
-                    duration = 200L
-                }
-                val scaleAnimatorY = ObjectAnimator.ofFloat(splashScreenView, View.SCALE_Y, 1f, 0f).apply {
-                    duration = 200L
-                }
-
-                // フェードアウト
-                val fadeOutAnimator = ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f).apply {
-                    duration = 200L
-                    interpolator = AccelerateInterpolator()
-                    doOnEnd { splashScreenViewProvider.remove() }
-                }
-
-                // run the animations in sequence
-                AnimatorSet().apply {
-                    playSequentially(rotationAnimator, AnimatorSet().apply {
-                        playTogether(scaleAnimatorX, scaleAnimatorY, fadeOutAnimator)
-                    })
-                    start()
-                }
+                startSplashAnimation(splashScreenViewProvider)
             }
         }
 
@@ -74,4 +53,60 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    // パーミッションを確認して，ない場合はリクエスト
+    private fun confirmPermission() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                } else -> {
+                // No location access granted.
+            }
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+    }
+
+    // Splashのアニメーション
+    private fun startSplashAnimation(splashScreenViewProvider: SplashScreenViewProvider) {
+        val splashScreenView = splashScreenViewProvider.iconView
+
+        // アイコンを回転
+        val rotationAnimator = ObjectAnimator.ofFloat(splashScreenView, View.ROTATION, 0f, 360f).apply {
+            duration = 800L
+        }
+
+        // スケール変更
+        val scaleAnimatorX= ObjectAnimator.ofFloat(splashScreenView, View.SCALE_X, 1f, 0f).apply {
+            duration = 200L
+        }
+        val scaleAnimatorY = ObjectAnimator.ofFloat(splashScreenView, View.SCALE_Y, 1f, 0f).apply {
+            duration = 200L
+        }
+
+        // フェードアウト
+        val fadeOutAnimator = ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f).apply {
+            duration = 200L
+            interpolator = AccelerateInterpolator()
+            doOnEnd { splashScreenViewProvider.remove() }
+        }
+
+        // run the animations in sequence
+        AnimatorSet().apply {
+            playSequentially(rotationAnimator, AnimatorSet().apply {
+                playTogether(scaleAnimatorX, scaleAnimatorY, fadeOutAnimator)
+            })
+            start()
+        }
+    }
+
 }
